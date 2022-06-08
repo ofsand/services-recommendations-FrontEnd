a16018e2b97551894534934726404bc02180f251
sbrika <somaya.brika@gmail.com>
2022-06-08 12:21:05 +0100

    transfert css to sass for add-service compt

--- src/app/services/data/services.service.ts
+++ src/app/services/data/services.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IService } from 'src/app/models/service.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
+  private _tokenTest = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6YWthcmlhIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2xvZ2luIiwiZXhwIjoxNjU3MTgzNTAxfQ.LG_orwbaTyCu8Lcjx_x1LA4BRwg_5rtBkuQ7lWJFBcg"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addService(service: IService) {
+    console.log(service)
+    return this.http.post('http://localhost:8080/api/admin/category/{idCategory}/services', 
+    service, {
+      headers: {'Authorization': this._tokenTest}
+    })
+  }
 }
