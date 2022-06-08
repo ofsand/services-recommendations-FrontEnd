9b66588ec247c951530d76a096d3584ddcb286c8
sbrika <somaya.brika@gmail.com>
2022-06-08 17:28:45 +0100

    create the service for adding serviceTradeperson

--- src/app/services-list/add-service/add-service.component.ts
+++ src/app/services-list/add-service/add-service.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Category } from 'src/app/models/category.model';
 import { IService } from 'src/app/models/service.model';
 import { ITradePerson } from 'src/app/models/tradeperson';
+import { CategoryService } from 'src/app/services/data/category.service';
+import { ServicesService } from 'src/app/services/data/services.service';
 
 @Component({
   selector: 'app-add-service',
@@ -13,16 +16,19 @@ export class AddServiceComponent implements OnInit {
 
   services !: IService;
   tradesperson !: ITradePerson;
-  
+  servicestradesperson !: any[];
+  categories !: Category[];
+  idCategory : number = 0;
+
   serviceForm = new FormGroup({
     email: new FormControl(null, Validators.required),
     title: new FormControl(''),
     location: new FormControl(''),
     phone: new FormControl(''),
-    category: new FormControl(''),
+    // category: new FormControl(''),
   });
 
-  persontradeForm = new FormGroup({
+  tradepersonForm = new FormGroup({
     email: new FormControl(null, Validators.required),
     title: new FormControl(''),
     location: new FormControl(''),
@@ -32,12 +38,23 @@ export class AddServiceComponent implements OnInit {
     domain: new FormControl(''),
     speciality: new FormControl(''),
     adress: new FormControl(''),
-    category: new FormControl(''),
+    // category: new FormControl(''),
   });
-  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
+  constructor(private modalService: NgbModal, private fb: FormBuilder, private servicesService : ServicesService, private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.servicesService.getAllServicesTradesPerson().subscribe( data => {
+      console.log("data : ",data);  
+      this.servicestradesperson=data;
+    });
+    this.categoryService.getAllCategories().subscribe(
+      (data) => {
+        this.categories = data
+      }
+    )
   }
+
+ 
   addTradePerson(contentTradeperson : any) {
     this.modalService.open(contentTradeperson, { centered: true });
   }
@@ -48,9 +65,16 @@ export class AddServiceComponent implements OnInit {
   onSubmitToAddService() {
     // TODO: Use EventEmitter with form value
     console.warn(this.serviceForm.value);
+    this.servicesService.addService(this.serviceForm.value,this.idCategory);
   }
-  onSubmitToAddPersontrade(){
-    console.warn(this.persontradeForm.value);
+  onSubmitToAddTradeperson(){
+    console.warn(this.tradepersonForm.value);
+    this.servicesService.addTradeperson(this.tradepersonForm.value,this.idCategory);
+  }
+
+  getCategory(event: any){
+      this.idCategory =event.target.value;
+      console.log("idCategory : ",this.idCategory); 
   }
 
 }
--- src/app/services/data/services.service.ts
+++ src/app/services/data/services.service.ts
@@ -1,20 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IService } from 'src/app/models/service.model';
+import { ServicesTradesPerson } from 'src/app/models/servicestradesperson.model';
+import { ITradePerson } from 'src/app/models/tradeperson';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
-  private _tokenTest = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6YWthcmlhIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2xvZ2luIiwiZXhwIjoxNjU3MTgzNTAxfQ.LG_orwbaTyCu8Lcjx_x1LA4BRwg_5rtBkuQ7lWJFBcg"
+  private _tokenTest = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6YWthcmlhIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2xvZ2luIiwiZXhwIjoxNjU3MjkzNzkxfQ.Vabglh6fpu8oAmaxJJLc6MeS_HHJXHjEYACyvCTZzXA"
+  url="http://localhost:8080/api/";
+
 
   constructor(private http: HttpClient) { }
-
-  addService(service: IService) {
+  
+  addService(service: IService, idCategory : number) {
     console.log(service)
-    return this.http.post('http://localhost:8080/api/admin/category/{idCategory}/services', 
-    service, {
+    this.http.post(this.url+"admin/category/"+idCategory+"/services", service, {
       headers: {'Authorization': this._tokenTest}
-    })
+    }).subscribe(res => {
+      console.log("Service added");
+    });
+  }
+
+  addTradeperson(tradeperson: ITradePerson, idCategory : number) {
+    console.log(tradeperson)
+    this.http.post(this.url+"admin/category/"+idCategory+"/tradesPerson", tradeperson, {
+      headers: {'Authorization': this._tokenTest}
+    }).subscribe(res => {
+      console.log("Service added");
+    });
+  }
+
+  getAllServicesTradesPerson() {
+    return this.http.get<ServicesTradesPerson[]>(this.url+'services-tradesPerson')
   }
 }
