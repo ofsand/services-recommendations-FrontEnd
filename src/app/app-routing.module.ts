1c62191b4211c8c55eac5091608a20753ad154cb
ZEDDOU <soufiane.a.zeddou@capgemini.com>
2022-06-09 10:09:57 +0100

    Modify Home and Login Pages Styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CategoriesComponent } from "./categories/categories.component";
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 import { RecommendationsComponent } from "./recommendations/recommendations.component";
 import { AddServiceComponent } from "./services-list/add-service/add-service.component";
 
@@ -9,8 +10,8 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'categories', component: CategoriesComponent},
   {path: 'recommendations', component: RecommendationsComponent},
-  {path: 'services', component: AddServiceComponent}
-  
+  {path: 'services', component: AddServiceComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
