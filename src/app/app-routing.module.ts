a2a12cd0bb38172d77d30aae521b28e389eb0526
CORP\felkhaml <fatiha2elkhamlichi@gmail.com>
2022-06-09 15:11:26 +0100

    navigate to service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { CategoriesComponent } from "./categories/categories.component";
 import { HomeComponent } from "./home/home.component";
 import { RecommendationsComponent } from "./recommendations/recommendations.component";
 import { AddServiceComponent } from "./services-list/add-service/add-service.component";
+import { ServiceItemComponent } from "./services-list/service-item/service-item.component";
+import { ServicesListComponent } from "./services-list/services-list.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'categories', component: CategoriesComponent},
   {path: 'recommendations', component: RecommendationsComponent},
-  {path: 'services', component: AddServiceComponent}
+  {path: 'services', component: AddServiceComponent},
+  {path: 'servicesDetails/:id', component: ServicesListComponent},
   
 ];
 
