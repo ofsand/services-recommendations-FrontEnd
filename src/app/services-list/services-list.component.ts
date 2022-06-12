abfd4b7e60fd396e7e202aae7f3455e5c52f42a0
sbrika <somaya.brika@gmail.com>
2022-06-12 19:03:46 +0100

    fix the problem of get number for service-list cmpt

--- src/app/services-list/services-list.component.ts
+++ src/app/services-list/services-list.component.ts
@@ -27,7 +27,7 @@ export class ServicesListComponent implements OnInit {
     email: new FormControl(null, Validators.required),
     title: new FormControl(''),
     location: new FormControl(''),
-    phone: new FormControl(''),
+    phoneNumber: new FormControl(''),
     description: new FormControl(''),
   });
 
@@ -35,7 +35,7 @@ export class ServicesListComponent implements OnInit {
     email: new FormControl(null, Validators.required),
     title: new FormControl(''),
     location: new FormControl(''),
-    phone: new FormControl(''),
+    phoneNumber: new FormControl(''),
     firstname: new FormControl(''),
     lastname: new FormControl(''),
     domain: new FormControl(''),
@@ -73,7 +73,7 @@ export class ServicesListComponent implements OnInit {
       (data: any) => {
         this.servicestradesperson = this.servicestradesperson.filter(servicetradeperson => (servicetradeperson.id !== data.id))
         this.servicestradesperson.push(data)
-      }
+      } 
     )
   
   }
